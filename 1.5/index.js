15a36efbfbdee70c2357ffec20ac3ac10ee5df54
Taras Hupalo <taras.hupalo@gmail.com>
2016-05-06 18:34:15 +0300

    Reimplement 1.5

--- 1.5/index.js
+++ 1.5/index.js
@@ -1,6 +1,19 @@
 function clone(obj) {
-  return JSON.parse(JSON.stringify(obj));
+  if (typeof obj === 'object') {
+    return Object.keys(obj).reduce(function reducer(acc, key) {
+      var result = acc; // introduce a new variable to avoid the ESLint error according to the no-param-reassign rule
+      result[key] = clone(obj[key]);
+      return result;
+    }, {});
+  }
+
+  return obj;
 }
 
+console.log(clone({ a: undefined }));
 console.log(clone({ a: 3, b: 5, c: 9 }));
 console.log(clone({ a: [1, 2, { s: 4, c: { u: 5 } }], s: 9 }));
+
+var a = { a: [1, 2, { s: 4, c: { u: 5 } }], s: 9 };
+var b = clone(a);
+console.log(b === a);
